cd464c403c1f931b11a6d5652607f9a74fb9b2ad
Samrat Subedi <samrat.subedi.77@gmail.com>
2025-03-06 01:03:26 +0545

    fixed with placeholder for pages

---
+++ src/app/admin/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contacts = () => {
+  return (
+    <div>Contacts Admin</div>
+  )
+}
+
+export default Contacts
---
+++ src/app/admin/services/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const services = () => {
+  return (
+    <div>services admin</div>
+  )
+}
+
+export default services
